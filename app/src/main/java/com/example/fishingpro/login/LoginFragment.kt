c642e6754f3e03891128c76276cdbed5712a89cc
Andrea Franco <andrea07021981@gmail.com>
2020-07-03 17:48:56 -0400

    Added login

---
+++ app/src/main/java/com/example/fishingpro/login/LoginFragment.kt
@@ -0,0 +1,19 @@
+package com.example.fishingpro.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.fishingpro.R
+
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
