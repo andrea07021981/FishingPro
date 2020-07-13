ab43ea8fdbd69dd96499cf773660a732e8a68516
Andrea Franco <andrea07021981@gmail.com>
2020-07-13 17:59:45 -0400

    Added home and navigation bottom

--- app/src/main/java/com/example/fishingpro/login/SignUpFragment.kt
+++ app/src/main/java/com/example/fishingpro/login/SignUpFragment.kt
@@ -1,20 +1,15 @@
 package com.example.fishingpro.login
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.fishingpro.EventObserver
-import com.example.fishingpro.R
 import com.example.fishingpro.constant.Authenticated
 import com.example.fishingpro.constant.Authenticating
 import com.example.fishingpro.constant.InvalidAuthentication
