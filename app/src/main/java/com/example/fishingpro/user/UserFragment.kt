a953d26c24560018e3213e1da1a1c42180ad335b
Andrea Franco <andrea07021981@gmail.com>
2020-07-16 18:28:39 -0400

    Added section weather

--- app/src/main/java/com/example/fishingpro/user/UserFragment.kt
+++ app/src/main/java/com/example/fishingpro/user/UserFragment.kt
@@ -5,15 +5,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
+import com.example.fishingpro.EventObserver
 import com.example.fishingpro.R
+import com.example.fishingpro.databinding.FragmentUserBinding
 
 class UserFragment : Fragment() {
+
+    private val userViewModel by viewModels<UserViewModel> {
+        UserViewModel.UserViewModelFactory()
+    }
+
+    private lateinit var dataBinding: FragmentUserBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_user, container, false)
+        dataBinding = FragmentUserBinding.inflate(inflater)
+        dataBinding.lifecycleOwner = this
+        dataBinding.userViewModel = userViewModel
+        dataBinding.maintoolbar.setNavigationOnClickListener {
+            userViewModel.backToRecipeList()
+        }
+        userViewModel.userEvent.observe(this.viewLifecycleOwner, EventObserver {
+            findNavController().popBackStack()
+        })
+        return dataBinding.root
     }
 }
--- app/src/main/java/com/example/fishingpro/user/UserViewModel.kt
+++ app/src/main/java/com/example/fishingpro/user/UserViewModel.kt
@@ -1,5 +1,35 @@
 package com.example.fishingpro.user
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.fishingpro.Event
+import com.example.fishingpro.login.SignUpViewModel
 
-class UserViewModel() : ViewModel()
+class UserViewModel(
+
+) : ViewModel() {
+
+    private val _userEvent = MutableLiveData<Event<Unit>>()
+    val userEvent: LiveData<Event<Unit>>
+        get() = _userEvent
+
+    fun backToRecipeList() {
+        _userEvent.value = Event(Unit)
+    }
+
+    /**
+     * Factory for constructing SignUpViewModel with parameter
+     */
+    class UserViewModelFactory(
+    ) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return UserViewModel() as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+}
