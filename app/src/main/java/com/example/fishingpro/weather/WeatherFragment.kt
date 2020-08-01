6ecdf0d969688850f47a4d63767e71ee7855ba21
Andrea Franco <andrea07021981@gmail.com>
2020-08-01 16:18:52 -0400

    Added transations; loading

--- app/src/main/java/com/example/fishingpro/weather/WeatherFragment.kt
+++ app/src/main/java/com/example/fishingpro/weather/WeatherFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.doOnPreDraw
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.ActivityNavigator
@@ -44,6 +45,8 @@ class WeatherFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        postponeEnterTransition()
+        view.doOnPreDraw { startPostponedEnterTransition() }
     }
     override fun onDestroy() {
         super.onDestroy()
