d0f78114a7ee92789595c309f2bdb4e288c85848
Andrea Franco <andrea07021981@gmail.com>
2020-08-24 18:56:16 -0400

    Added repository for catches; Data model for catches; update Calendar VM

--- app/src/main/java/com/example/fishingpro/data/source/remote/datasource/CalendarRemoteDataSource.kt
+++ app/src/main/java/com/example/fishingpro/data/source/remote/datasource/CalendarRemoteDataSource.kt
@@ -1,15 +1,32 @@
 package com.example.fishingpro.data.source.remote.datasource
 
+import android.util.Log
 import com.example.fishingpro.data.Result
+import com.example.fishingpro.data.await
+import com.example.fishingpro.data.domain.LocalCatch
 import com.example.fishingpro.data.source.CalendarSource
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreException
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.lang.Exception
 import javax.inject.Inject
 
 class CalendarRemoteDataSource @Inject constructor(
     val firestore: FirebaseFirestore
 ) : CalendarSource {
-    override suspend fun retrieveCatches(userId: String): Flow<Result<List<Unit>>> {
-        TODO("Not yet implemented, change unit with data")
+    override suspend fun retrieveCatches(userId: String): Flow<Result<List<LocalCatch?>>> = flow {
+        emit(Result.Loading)
+        try {
+            val request = firestore.collection("Catch").whereEqualTo("UserId", userId).get().await()
+            val dataMapped = request.documents.map {
+                val singleCatch = it.toObject(LocalCatch::class.java)
+                singleCatch?.CatchId = it.id
+                singleCatch
+            }
+            emit(Result.Success(dataMapped))
+        } catch (e: FirebaseFirestoreException) {
+            emit(Result.ExError(e))
+        }
     }
 }
